d03003097b14fba0817bfb6a081a6fe6468294d2
Eden Bensaid <edenoosh15@gmail.com>
2020-12-22 19:16:20 -0800

    removed # from URL address

--- client/src/main.js
+++ client/src/main.js
@@ -8,6 +8,7 @@ Vue.config.productionTip = false
 Vue.use(VueRouter)
 
 const router = new VueRouter({
+  mode: 'history',
   routes: [
     { path: '/:storyid', name: 'story', component: Editor },
   ]
