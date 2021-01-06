f868a2590d244ce2d48e66ad4730a4dc4bd5e0af
Eden Bensaid <edenoosh15@gmail.com>
2021-01-05 20:26:15 -0800

    adding more preset titles

--- client/src/components/Constants.js
+++ client/src/components/Constants.js
@@ -1,10 +1,10 @@
 export default {
     // If IMAGE_PATH cahnges, need to manually change default stories img tags in backend/outputs. 
     IMAGE_PATH: "unsplash25k/sketch_images1024/",
-    PRESET_TITLES: ["The Princess of the North", "The Delightful Prince", "The Happy Hippo", "The Adventures of Layla", "A Tale of Two Sisters",
+    PRESET_TITLES: ["The Plague of the Century", "A Mysterious Angel", "The Unforgettable Sunset", "The Great Sunny Day", "Go To Distance", "I'm in Love", "The Spell under the Ocean", "The Princess of the North", "The Delightful Prince", "The Happy Hippo", "The Adventures of Layla", "A Tale of Two Sisters",
         "The Hummus Lady", "Party in the West Coast", "Everything changed at Sunset", "The Cold is Coming", "Playing it Safe", "Adventures in Africa",
         "The Eagle of the West", "The Trees Under the Sun", "Under the Shining Sun", "Someone To Trust", "You Only Have Three Wishes", "A Little Piece of Advice",
-        "Tasting the Ocean", "Where are my Keys?", "The Curios Cat", "The Spring of 2050", "No More Fighting", "The Last Days of the King", "The Castle by the Forest"],
+        "Behind the Curtain", "The Curios Cat", "The Spring of 2050", "No More Fighting", "The Last Days of the King", "The Castle by the Forest", "Singing in the Rain"],
     PRESET_IMG_IDS: ["_aFgVTAfKNg", "_583eiIT_84", "_b1BFtvb8Qs", "_aXnfxSW_e0", "_FacZj2pLAc",
         "_gycpm2K900", "-BkZzidZIZg", "__G2yFuW7jQ", "_e437sL_W2o", "-y-v-wi0kHU"]
 }
