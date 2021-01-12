d952369fbed64d53a827bca38ebf95ab9ac69568
Eden Bensaid <edenoosh15@gmail.com>
2021-01-11 21:45:16 -0800

    updated image-caption and added a prompt

--- client/src/components/Constants.js
+++ client/src/components/Constants.js
@@ -2,7 +2,7 @@ export default {
     // If IMAGE_PATH cahnges, need to manually change default stories img tags in backend/outputs. 
     IMAGE_PATH: "unsplash25k/sketch_images1024/",
     PRESET_TITLES: ["The Loving Cat", "The Enchanted Princess", "The Birth of a New Universe", "The Fairy", "The Plague of the Century", "A Mysterious Angel", "The Unforgettable Sunset", "The Great Sunny Day", "The Spell under the Ocean", "The Princess of the North", "The Delightful Prince", "The Happy Hippo", "The Adventures of Layla", "A Tale of Two Sisters",
-        "The Hummus Lady", "Party in the West Coast", "Everything changed at Sunset", "The Cold is Coming", "Adventures in Africa", "The Dragons Village", "Lost in the Forest",
+        "The Hummus Lady", "Party in the West Coast", "Everything changed at Sunset", "The Cold is Coming", "Adventures in Africa", "The Dragons Village", "Lost in the Forest", "The Pirates Bay",
         "The Eagle of the West", "The Trees Under the Sun", "Under the Shining Sun", "Someone To Trust", "You Only Have Three Wishes", "A Little Piece of Advice", "The Hall of Justice",
         "Behind the Curtain", "The Curios Cat", "The Spring of 2050", "No More Fighting", "The Last Days of the King", "The Castle by the Forest", "The Malicious Genie", "The Three Knights"],
     PRESET_IMG_IDS: ["_aFgVTAfKNg", "_583eiIT_84", "_b1BFtvb8Qs", "_aXnfxSW_e0", "_FacZj2pLAc",
