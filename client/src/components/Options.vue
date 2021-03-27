eb848c3ddfc7f7656e3504387449c19e5ebee2db
EdenBD <edenoosh15@gmail.com>
2021-03-27 12:51:07 +0300

    reduced img quality for faster loading

--- client/src/components/Options.vue
+++ client/src/components/Options.vue
@@ -11,7 +11,7 @@
               <v-list-item-icon>
                 <v-icon>mdi-text</v-icon>
               </v-list-item-icon>
-              <v-list-item-title v-text="text" style="{'padding-left': '0.1em';}"></v-list-item-title>
+              <v-list-item-title v-text="text"></v-list-item-title>
             </v-list-item>
           </div>
         </v-list-item-group>
@@ -27,7 +27,7 @@
             class="options-img"
           >
             <v-list-item-title>
-              <v-img :src="`${images_path}${img}/256x256`"></v-img>
+              <v-img :src="`${images_path}${img}/90x90`"></v-img>
             </v-list-item-title>
           </v-list-item>
         </v-list-item-group>
